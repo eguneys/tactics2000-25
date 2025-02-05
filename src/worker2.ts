0fba6d39f5354cd49cfabbe44ca3a23e108b7b33
Emre Guneyler <eguneys@gmail.com>
2025-02-05 18:17:21 +0300

    filter no

--- src/worker2.ts
+++ src/worker2.ts
@@ -52,7 +52,7 @@ function work_while_checking() {
 
     puzzles = puzzles.filter(_ => _.sans[0].includes('B'))
     puzzles = puzzles.filter(_ => !_.tags['mate'] && !_.tags['endgame'])
-    puzzles = puzzles
+    //puzzles = puzzles.filter(_ => _.id === '00Uwn')
 
     for (let i = 0; i < puzzles.length; i++) {
         if (i % 10 === 0) {
